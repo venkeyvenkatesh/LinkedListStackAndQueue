439489b4341c7269fc31cb5aaf29372d97367cf4
venkey <venkeyvenkatesh885@gmail.com>
2020-10-22 02:42:59 +0530

    first commit

---
+++ LinkedListSatckAndQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListSatckAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Demo program on Linked List stack and Queue");
+        }
+    }
+}
