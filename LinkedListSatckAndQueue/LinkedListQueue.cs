9225492d81cfc5e6a8594511a95c41645abe7069
venkey <venkeyvenkatesh885@gmail.com>
2020-10-22 03:04:34 +0530

    [venkey]added the enqueue method to enqueue into the queue and displaying the elements

---
+++ LinkedListSatckAndQueue/LinkedListQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListSatckAndQueue
+{
+    class LinkedListQueue
+    {
+        internal Node head;
+        public LinkedListQueue()
+        {
+            this.head = null;
+        }
+        internal void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            if (head==null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+        }
+        internal void Display()
+        {
+            if (head==null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " \t");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- LinkedListSatckAndQueue/Program.cs
+++ LinkedListSatckAndQueue/Program.cs
@@ -7,22 +7,13 @@ namespace LinkedListSatckAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Demo program on Linked List stack and Queue");
-            LinkedListStack stack = new LinkedListStack();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
-            if (stack.isEmpty())
-            {
-                Console.WriteLine("\nStack is empty");
-            }
-            else
-            {
-                Console.WriteLine("\nStack is not empty");
-            }
+
+
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(50);
+            queue.Enqueue(60);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
